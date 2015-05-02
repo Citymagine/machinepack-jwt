96d4fa5bed6b976f5210f1fb744dcd04d4ac3116
Scott Prue <sprue.dev@gmail.com>
2015-05-01 21:01:37 -0400

    Initial commit. Basic encode and decode machines added.

---
+++ machines/decode.js
@@ -0,0 +1,57 @@
+module.exports = {
+
+
+  friendlyName: 'decode',
+
+
+  description: 'Decode a JWT.',
+
+
+  extendedDescription: 'Decode a JSON web token with a provided secret. You may choose what algorithm you want to use to decode the JWT, but make sure to use the same algorithm that you used to encode the JWT.',
+
+
+  inputs: {
+    secret: {
+      example: 'abc123jdhs3h4js',
+      description: 'Secret used to decode the JSON web token.',
+      required: true
+    },
+    token: {
+      example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ',
+      description: 'JSON web token to decode.',
+      required: true
+    },
+    algorithm:{
+      example:'HS256',
+      description:'The type of algorithm that is used to decode the JWT. Options: HS256, HS384, HS512 and RS256. Make sure to use the same algorithm that you used to encode the JWT.'
+    }
+  },
+
+
+  defaultExit: 'success',
+
+
+  exits: {
+
+    error: {
+      description: 'Unexpected error occurred while decoding JWT.',
+    },
+
+    success: {
+      description: 'JWT decoded successfully.',
+    },
+
+  },
+
+
+  fn: function (inputs,exits) {
+    var jwt = require('jwt-simple');
+    if(){
+
+    }
+    return exits.success(jwt.decode(inputs.token, inputs.secret));
+  },
+
+
+
+};
---
+++ machines/encode.js
@@ -0,0 +1,57 @@
+module.exports = {
+
+
+  friendlyName: 'encode',
+
+
+  description: 'Encode a JWT.',
+
+
+  extendedDescription: 'Encode a JSON web token with a provided secret. You may choose what algorithm you want to use to encode the JWT, but make sure to use the same algorithm when you are decoding the JWT.',
+
+
+  inputs: {
+    secret: {
+      example: 'abc123jdhs3h4js',
+      description: 'The secret used to encode the JWT.',
+      required: true
+    },
+    payload: {
+      example: {username:'ExampleUser', email:'example@email.com'},
+      description: 'The contents of the JWT.',
+      required: true
+    },
+    algorithm:{
+      example:'HS256',
+      description:'The type of algorithm that is used to encode the JWT. Options: HS256, HS384, HS512 and RS256.'
+    }
+  },
+
+  defaultExit: 'success',
+
+
+  exits: {
+
+    error: {
+      description: 'Unexpected error occurred while encoding JWT.',
+    },
+
+    success: {
+      description: 'JWT encoded successfully.',
+    },
+
+  },
+
+
+  fn: function (inputs,exits) {
+    var jwt = require('jwt-simple');
+    var a = "HS256";
+    if(inputs.algorithm){
+      a = inputs.algorithm
+    }
+    return exits.success(jwt.encode(inputs.payload, inputs.secret, a));
+  },
+
+
+
+};
